1be36877624b5cf8d214c849068caf3a9df5537e
Mario Maranda <274343743+ineedabuff@users.noreply.github.com>
2026-06-26 03:00:44 -0400

    feat(analysis): detect home subvolume

--- installer/app/modules/system.py
+++ installer/app/modules/system.py
@@ -1,5 +1,6 @@
 from app.analysis.filesystem import FilesystemAnalysis
 from app.analysis.firmware_analysis import FirmwareAnalysis
+from app.analysis.home_subvolume_analysis import HomeSubvolumeAnalysis
 from app.analysis.operating_system_analysis import OperatingSystemAnalysis
 from app.analysis.root_subvolume_analysis import RootSubvolumeAnalysis
 from app.modules.base import Module
@@ -17,4 +18,5 @@ class SystemCheckModule(Module):
             FirmwareAnalysis().run(),
             FilesystemAnalysis().run(),
             RootSubvolumeAnalysis().run(),
+            HomeSubvolumeAnalysis().run(),
         ]
