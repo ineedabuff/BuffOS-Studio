04a073dad7c21b21a7ec90134ac7c69f70d0ce22
Mario Maranda <274343743+ineedabuff@users.noreply.github.com>
2026-06-28 03:00:44 -0400

    Add Btrfs doctor check

---
+++ tests/test_btrfs_check.py
@@ -0,0 +1,16 @@
+from app.checks.storage.btrfs import BtrfsCheck
+
+
+class FakeProvider:
+    class Result:
+        stdout = "btrfs\n"
+
+    def run(self, *args, **kwargs):
+        return self.Result()
+
+
+def test_btrfs_check():
+    result = BtrfsCheck(FakeProvider()).run()
+
+    assert result.passed is True
+    assert result.title == "Btrfs"
